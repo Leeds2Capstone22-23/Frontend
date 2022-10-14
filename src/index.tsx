a6db4975d54ce62148a6a57576918e892e4d64bc
Jaryd Meek <jarydmeek@gmail.com>
2022-10-14 00:48:48 -0600

    Fixed test cases for CI/CD

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
@@ -12,9 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={defaultStore}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <App />
     </Provider>
   </React.StrictMode>,
 );
