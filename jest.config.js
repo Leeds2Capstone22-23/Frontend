f4b0e913d89781f301117dfeca23a89b9413124b
Jaryd Meek <jarydmeek@gmail.com>
2022-10-15 18:29:19 -0600

    Fixed jest test running for coverage tests

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,11 @@
-/** @type {import('jest').Config} */
-const config = {
-    coverageThreshold: {
-      global: {
-        branches: 50,
-        functions: 50,
-        lines: 50,
-      },
-    },
-  };
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/mocks/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/mocks/styleMock.js',
+  },
   
-  module.exports = config;
+};
