daa49dee374bf2671d2eba5a40d14c50f3a41326
Jaryd Meek <jarydmeek@gmail.com>
2022-11-26 23:05:01 -0700

    Basic Auth Flow

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import AuthHandler from './components/AuthHandler';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { defaultStore } from './redux';
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={defaultStore}>
-        <App />
+        <AuthHandler />
     </Provider>
   </React.StrictMode>,
 );
---
+++ src/redux/hooks/authHook.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootStore } from '..';
+import { Status } from '../../types/types';
+/**
+ * STATUS
+ */
+export function AuthStatus() {
+  return useSelector((state: RootStore) => state.authStatusReducer);
+}
+
+/**
+ * DATA
+ */
+export function AuthData(forceRefresh = false) {
+  const userAuthData = useSelector((state: RootStore) => state.authDataReducer);
+  const userAuthStatus = useSelector((state: RootStore) => state.authStatusReducer);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // If we haven't loaded data or data loading has failed
+    if (userAuthStatus === Status.Initial || userAuthStatus === Status.Failed || forceRefresh) {
+        //If userID != -1, check to make sure it's a valid account, and fetch updated data
+
+        // otherwise try and load from local storage
+        
+    } else if (userAuthStatus === Status.Succeeded) {
+        // save to storage
+    }
+  }, [dispatch, forceRefresh]);
+  return userAuthData;
+}
