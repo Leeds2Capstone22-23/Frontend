a2459841940ed638297c8963cdf0fa7aba369443
IsabelAnaya <56010497+IsabelAnaya@users.noreply.github.com>
2023-02-08 22:45:31 -0700

    NavBar default story
    Default Store story shows what the current local user sees.

--- src/stories/NavBar.stories.tsx
+++ src/stories/NavBar.stories.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { BrowserRouter } from 'react-router-dom';
+
 import NavBar from '../components/NavBar';
+import { Provider } from 'react-redux';
+import { defaultStore } from '.././redux';
+
 
 export default {
   component: NavBar,
@@ -8,6 +13,10 @@ export default {
 
 const Template: ComponentStory<typeof NavBar> = (args) => <NavBar {...args} />;
 
-export const Thing = Template.bind({});
+// uses the default store (whatever it currently looks like in the actual app, i.e. who's logged in and what labels they have)
+export const DefaultStore = Template.bind({});
+DefaultStore.decorators = [
+  (story) => <Provider store={defaultStore}><BrowserRouter>{story()}</BrowserRouter></Provider>
+]
 
-//TEMP! GET THIS WORKING
+// todo: logged out? all labels selected
