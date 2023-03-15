e0bb6b47d93bfc960c8df6d15c8d99f6d8f2e0b9
IsabelAnaya <56010497+IsabelAnaya@users.noreply.github.com>
2023-03-14 18:39:04 -0600

    Change API URL back

--- src/logic/apiRequest.tsx
+++ src/logic/apiRequest.tsx
@@ -32,8 +32,7 @@ export async function fetchData(
   setErrors?:Function,
   auth?:string,
 ) {
-  const url = 'http://localhost:8080/v1/graphql';
-  // const url = 'https://leeds.jaryd.io/api/v1/';
+  const url = 'https://leeds.jaryd.io/api/v1/';
   const user = defaultStore.getState().rootReducer.authDataReducer;
   const response = await fetch(
     `${url}`,
