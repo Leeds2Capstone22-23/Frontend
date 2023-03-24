6a0b8a8a7575f217dde93a81ee23bf68bed1b587
IsabelAnaya <56010497+IsabelAnaya@users.noreply.github.com>
2023-03-24 10:42:23 -0600

    Comments in docView

--- src/components/documents/documentView.tsx
+++ src/components/documents/documentView.tsx
@@ -130,7 +130,7 @@ export default function DocumentView() {
   // -----
   // USE EFFECT
   // -----
-  // Add the event listener for highlighted text to the window on load
+  // Add the event listener for highlighted text to the window on load (detect highlights)
   useEffect(() => {
     ['mouseup', 'dblclick'].forEach((curr) => {
       window.addEventListener(curr, (event) => {
@@ -298,10 +298,12 @@ export default function DocumentView() {
       </Box>
     );
   } else {
+    // "Add Snippet" button when you highlight text
     snippetOptions = (
       <Button
         onClick={async () => {
           setShowSnippetCreationOptions(true);
+          // will get the first instance of the highlighted string in the document
           const offset = currDocument.content!
             .indexOf(
               window.getSelection()?.getRangeAt(0)!.toString()!.replaceAll('\n\n', '\n')!,
@@ -321,7 +323,6 @@ export default function DocumentView() {
               length: (window.getSelection()!.toString().length),
               // This is not the right way to do this, but I'm leaving it for now
               // TODO: find a better way to grab index
-              // this searches for the first instance of the first letter of the highlight. :( !
               char_offset: currDocument.content!
                 .replaceAll('\n', '\n\n')
                 .indexOf(window.getSelection()?.getRangeAt(0)!.toString()!),
@@ -335,6 +336,7 @@ export default function DocumentView() {
     );
   }
 
+  // actual components and html rendered
   if (documentStatus === Status.Succeeded && currDocument) {
     return (
         <>
@@ -401,11 +403,15 @@ export default function DocumentView() {
             </Paper>
         </>
     );
+
+  // no document found
   } if (documentStatus === Status.Succeeded) {
     return (
         <Page404 />
     );
   }
+
+  // loading
   return (
         <div style={{
           textAlign: 'center',
